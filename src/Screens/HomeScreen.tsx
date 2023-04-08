209cd1baae8713275aebe8270ea8e90650cbd727
safin-ghoghabori <safin.ghoghabori@cactusglobal.com>
2023-04-08 12:58:25 +0530

    added searchbar and grid

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -12,10 +12,10 @@ const HomeScreen = () => {
 
   return (
     <ScrollView>
-      <SafeAreaView>
+      <SafeAreaView style={{ paddingVertical: 24, gap: 24 }}>
+        {/* Header */}
         <View
           style={{
-            paddingVertical: 24,
             paddingHorizontal: 24,
             flexDirection: "row",
             alignItems: "center",
@@ -27,7 +27,7 @@ const HomeScreen = () => {
             style={{ width: 52, aspectRatio: 1, borderRadius: 52 }}
           />
 
-          <View>
+          <View style={{ flex: 1 }}>
             <Text
               style={{
                 fontSize: 18,
@@ -37,10 +37,10 @@ const HomeScreen = () => {
               }}
               numberOfLines={1}
             >
-              Hi, James{" "}
+              Hi, James 👋{" "}
             </Text>
             <Text
-              style={{ color: colors.text, opacity: 0.75 }}
+              style={{ color: colors.text, opacity: 0.75, fontSize: 11 }}
               numberOfLines={1}
             >
               Discover fashions that suit your style
@@ -66,16 +66,81 @@ const HomeScreen = () => {
         <View style={{ flexDirection: "row", paddingHorizontal: 24, gap: 24 }}>
           <TouchableOpacity
             style={{
+              flex: 1,
+              flexDirection: "row",
               borderWidth: 1,
               height: 52,
               borderRadius: 52,
               borderColor: colors.border,
               alignItems: "center",
-              flex: 1,
               gap: 12,
               paddingHorizontal: 24,
             }}
-          ></TouchableOpacity>
+          >
+            <Icons
+              name="search"
+              size={24}
+              color={colors.text}
+              style={{ opacity: 0.5 }}
+            />
+            <Text
+              style={{
+                flex: 1,
+                opacity: 0.5,
+                fontSize: 16,
+                color: colors.text,
+              }}
+            >
+              Search
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              width: 52,
+              aspectRatio: 1,
+              backgroundColor: "black",
+              borderRadius: 52,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Icons name="tune" size={24} color={colors.background} />
+          </TouchableOpacity>
+        </View>
+
+        {/* Grid View Area */}
+        <View
+          style={{
+            paddingHorizontal: 24,
+          }}
+        >
+          {/* Title */}
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ fontSize: 18, fontWeight: "600" }}>
+              New Collections
+            </Text>
+            <TouchableOpacity>
+              <Text>See all</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Card */}
+          <View style={{ flexDirection: "row", height: 200, gap: 12 }}>
+            <View style={{ flex: 1 }}>
+              <Card />
+            </View>
+            <View style={{ flex: 1, gap: 12 }}>
+              <Card />
+              <Card />
+            </View>
+          </View>
         </View>
       </SafeAreaView>
     </ScrollView>
@@ -83,3 +148,30 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+const Card = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        position: "relative",
+        overflow: "hidden",
+        borderRadius: 24,
+      }}
+    >
+      <Image
+        source={{
+          uri: "https://images.unsplash.com/photo-1534030347209-467a5b0ad3e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+        }}
+        resizeMode="cover"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+        }}
+      />
+    </View>
+  );
+};
