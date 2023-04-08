45d8dba2905c325c050b3aac32fd385a4f5d2fdc
safin-ghoghabori <safin.ghoghabori@cactusglobal.com>
2023-04-08 10:47:45 +0530

    updated root component file

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import HomeScreen from "./src/Screens/HomeScreen";
+import DetailsScreen from "./src/Screens/DetailsScreen";
+import RootNavigation from "./src/navigation/RootNavigation";
+import { useMemo } from "react";
 
 export default function App() {
+  console.log("default...", DefaultTheme);
+  const theme = useMemo(
+    () => ({
+      ...DefaultTheme,
+      colors: {
+        ...DefaultTheme.colors,
+        background: "#f5f5f5",
+        text: "#191919",
+        border: "#D9D9D9",
+        primary: "#191919",
+      },
+    }),
+    []
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <RootNavigation />
+        <StatusBar style="dark" />
+      </NavigationContainer>
     </View>
   );
 }
@@ -13,8 +36,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
